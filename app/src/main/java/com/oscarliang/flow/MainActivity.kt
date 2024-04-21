c28099e4f329cf0166307228e90bc2ac619d60eb
OscarLiang <a7757729@gmail.com>
2024-04-21 13:01:40 +0800

    Add edge to edge

--- app/src/main/java/com/oscarliang/flow/MainActivity.kt
+++ app/src/main/java/com/oscarliang/flow/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.oscarliang.flow
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.oscarliang.flow.databinding.ActivityMainBinding
@@ -12,17 +17,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupWindow()
+        setNavController()
+    }
 
+    private fun setupWindow() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemWindowInsets = insets.getInsets(
+                WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.ime()
+            )
+            binding.root.updatePadding(
+                left = systemWindowInsets.left,
+                right = systemWindowInsets.right,
+            )
+            binding.appbar.updatePadding(
+                top = systemWindowInsets.top,
+            )
+            insets
+        }
+    }
+
+    private fun setNavController() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             (destination.id != R.id.newsDetailFragment).let {
-                binding.toolbar.isVisible = it
+                binding.appbar.isVisible = it
                 binding.bottomNav.isVisible = it
             }
         }
