e46ebfc7ff9989b20dfbf8afc6a4467909d38cdb
OscarLiang <a7757729@gmail.com>
2024-05-03 18:25:08 +0800

    Clean up

--- app/src/main/java/com/oscarliang/flow/util/Constants.kt
+++ app/src/main/java/com/oscarliang/flow/util/Constants.kt
@@ -1,7 +1,6 @@
 package com.oscarliang.flow.util
 
 const val API_KEY = "c5516eeb-b7ce-45c5-b21b-5588a8d670f4"
-const val AD_ID = "ca-app-pub-3940256099942544/2247696110"
 const val NEWS_URL = "https://eventregistry.org/"
 const val DB_NAME = "news.db"
 const val PREFERENCE_NAME = "user_preferences"
